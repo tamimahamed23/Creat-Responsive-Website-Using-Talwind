86a54b46e354cc0c7a969ee9b2f92e28d83cd5e5
ahamed-tamim <ahamedtamim790@gmail.com>
2024-01-21 13:06:35 +0600

    complite my header section now i will create main section

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+
+        fontFamily: {
+            
+
+            'Manrope':['sans-serif']
+
+
+          },
+       
+      },
+    },
+    plugins: [],
+  }
